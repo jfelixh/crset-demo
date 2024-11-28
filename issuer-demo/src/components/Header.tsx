88f37e24d17d63cb5574229fb6868fd593e5ec95
ichan-yeong <chan9908181@gmail.com>
2024-11-28 12:29:15 +0100

    created a login frontend

--- issuer-demo/src/components/Header.tsx
+++ issuer-demo/src/components/Header.tsx
@@ -3,37 +3,28 @@ import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 
-const Header = ({ showLinks }: { showLinks: boolean }) => (
-    <header className="bg-gray-800 text-white p-4">
-      <nav className="flex gap-4">
-        {showLinks && (
-            <>
-              <Link
-                  href="/credentialIssuance"
-                  className={cn(buttonVariants({ variant: "ghost" }), "text-white")}
-              >
-                Verifiable Credential Issuance
-              </Link>
-              <Link
-                  href="/employees"
-                  className={cn(buttonVariants({ variant: "ghost" }), "text-white")}
-              >
-                Employee List
-              </Link>
-            </>
-        )}
-          {!showLinks && (
-              <>
-                  <Link
-                      href="/logIn"
-                      className={cn(buttonVariants({ variant: "ghost" }), "text-white")}
-                  >
-                      Log In
-                  </Link>
-              </>
-          )}
-      </nav>
-    </header>
-);
-
+const Header = ({ showLinks }: { showLinks: boolean }) => {
+    return (
+        <>
+            {showLinks && (
+                <header className="bg-gray-800 text-white p-4">
+                    <nav className="flex gap-4">
+                        <Link
+                            href="/credentialIssuance"
+                            className={cn(buttonVariants({ variant: "ghost" }), "text-white")}
+                        >
+                            Verifiable Credential Issuance
+                        </Link>
+                        <Link
+                            href="/employees"
+                            className={cn(buttonVariants({ variant: "ghost" }), "text-white")}
+                        >
+                            Employee List
+                        </Link>
+                    </nav>
+                </header>
+            )}
+        </>
+    );
+};
 export default Header;
