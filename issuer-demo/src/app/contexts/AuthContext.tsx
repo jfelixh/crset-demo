503bc2cf6aaa3b0f1e5da68c47c95bb06d0709c0
ichan-yeong <chan9908181@gmail.com>
2025-01-22 15:16:03 +0100

    commit first

--- issuer-demo/src/app/contexts/AuthContext.tsx
+++ issuer-demo/src/app/contexts/AuthContext.tsx
@@ -36,6 +36,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const logout = () => {
     localStorage.removeItem("authToken");
     setIsAuthenticated(false);
+    document.cookie = "authToken=; Path=/; SameSite=Lax; Max-Age=0;";
     router.push("/");
   };
 
