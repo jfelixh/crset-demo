a7275c039540d82570be3a6071980a73aad99fff
Natalia Milanova <ge65was@mytum.de>
2025-02-11 20:33:02 +0100

    small fixes for the demo

--- issuer-demo/src/app/api/exchangeRate/route.ts
+++ issuer-demo/src/app/api/exchangeRate/route.ts
@@ -1,17 +1,10 @@
 export const GET = async () => {
   console.log("Fetching exchange rate...");
   try {
-    // const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd");
-    // const data = await response.json();
-    // console.log("Received data:", data);
-    // return new Response(JSON.stringify({ price: data.ethereum?.usd }), {
-    //   status: 200,
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // });
-    // Revert for the demo (Use hardcoded value because there is a limit)
-    return new Response(JSON.stringify({ price: 3265.74 }), {
+    const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd");
+    const data = await response.json();
+    console.log("Received data:", data);
+    return new Response(JSON.stringify({ price: data.ethereum?.usd }), {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
@@ -19,9 +12,16 @@ export const GET = async () => {
     });
   } catch (error) {
     console.error("Error fetching exchange rate:", error);
-    return new Response(
-      JSON.stringify({ error: "Failed to fetch exchange rate" }),
-      { status: 500, headers: { 'Content-Type': 'application/json' } }
-    );
+     // Revert after the demo (Use hardcoded value because if something fails)
+    // return new Response(
+    //   JSON.stringify({ error: "Failed to fetch exchange rate" }),
+    //   { status: 500, headers: { 'Content-Type': 'application/json' } }
+    // );
+    return new Response(JSON.stringify({ price: 3265.74 }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 };
