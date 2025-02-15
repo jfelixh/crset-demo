e230e18b1b6fe68a5ccdfdd88a80fe0c69c87463
Felix Hoops <9974641+jfelixh@users.noreply.github.com>
2025-02-15 23:58:00 +0100

    Revamped homepage
    Signed-off-by: Felix Hoops <9974641+jfelixh@users.noreply.github.com>

--- issuer-demo/src/app/page.tsx
+++ issuer-demo/src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/legacy/image";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
@@ -12,12 +11,7 @@ export default function LoginPage() {
         <div className="container flex justify-center">
           <div>
             <div className="flex flex-col items-center">
-              <Image
-                src="/images/teamwork-vector-illustration-style_717774-90944-removebg-preview.png"
-                alt="Teamwork Vector Illustration Style"
-                width={500}
-                height={500}
-              />
+              <div className="text-[12rem] mb-8">🪪</div>
               <Button
                 className="w-full bg-blue-800"
                 onClick={() => router.push("/credentialIssuance")}
@@ -29,21 +23,14 @@ export default function LoginPage() {
         </div>
       </div>
 
-      <div className="w-1/2 relative flex flex-col justify-center items-center">
-        <Image
-          src="/images/loginBackGroundImage.png"
-          alt="LogIn Image"
-          layout="fill"
-          objectFit="cover"
-          className="absolute z-0"
-        />
-        <h1 className="relative z-10 text-4xl font-bold text-center font-serif">
+      <div className="w-1/2 relative flex flex-col justify-center items-center bg-gradient-to-br from-gray-900 via-black to-gray-900">
+        <h1 className="text-4xl font-bold text-center font-serif text-white">
           ACME Inc.
           <br />
           Staff Management System
         </h1>
         <br />
-        <h2 className="relative z-10 text-2xl font-medium text-center italic tracking-wide">
+        <h2 className="text-2xl font-medium text-center italic tracking-wide text-white/80">
           Issue employee credentials <br />
           and revoke them when necessary.
         </h2>
