ed7e816a4c7b83716386209719c54833f087dde1
Felix Hoops <9974641+jfelixh@users.noreply.github.com>
2025-02-17 15:15:24 +0100

    Containerized verifier backend build
    Signed-off-by: Felix Hoops <9974641+jfelixh@users.noreply.github.com>

--- verifier-demo/server/src/index.ts
+++ verifier-demo/server/src/index.ts
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
+    //TODO: Change to the dynamic frontend URL
     origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
