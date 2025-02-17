ad581839509920c4beb17c37198e464bd2b7873d
Felix Hoops <9974641+jfelixh@users.noreply.github.com>
2025-02-17 16:10:58 +0100

    Updated eslint config
    Signed-off-by: Felix Hoops <9974641+jfelixh@users.noreply.github.com>

--- verifier-demo/server/eslint.config.mjs
+++ verifier-demo/server/eslint.config.mjs
@@ -4,8 +4,16 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    ignores: ["dist/", "tsconfig.json"]
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", caughtErrors: 'none' }],
+    },
+  },
 ];
