0368438943e1fbb750eb5764e1eb5d8ca6272f6c
Felix Hoops <9974641+jfelixh@users.noreply.github.com>
2025-02-18 19:53:04 +0100

    Removed unused file
    Signed-off-by: Felix Hoops <9974641+jfelixh@users.noreply.github.com>

--- verifier-demo/server/src/lib/verifyPresentation.ts
+++ verifier-demo/server/src/lib/verifyPresentation.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright 2024 Software Engineering for Business Information Systems (sebis) <matthes@tum.de> .
- * SPDX-License-Identifier: MIT
- */
-
 import {
   verifyCredential,
   verifyPresentation,
